4711d06ce6f9694ebc6bc2ef92e09a03d3ee29a0
rssarto <ricardo.soares.sarto@gmail.com>
2018-04-11 12:11:28 -0300

    Tests with Streams

---
+++ java8tests-app/src/main/java/test/streams/TestStreams.java
@@ -0,0 +1,15 @@
+package test.streams;
+
+public class TestStreams {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static void filterPersonStreamByAge() {
+		
+		
+	}
+	
+}
