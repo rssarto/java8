3a3a061f29fcb426d142a859470902eecf4749b8
root <root@localhost.localdomain>
2019-11-03 09:42:27 -0300

    replacing uuid with matcher

--- java8tests-app/src/main/java/test/scanner/ScannerRegexReplace.java
+++ java8tests-app/src/main/java/test/scanner/ScannerRegexReplace.java
@@ -22,6 +22,7 @@ public class ScannerRegexReplace {
             "                \"configurable\": false,\n" +
             "                \"moveable\": false,\n" +
             "                \"mirrorOn\": {\n" +
+            "                \"dependentFields\": [\"143d3314-43b2-4711-899f-843e223d1128\", \"db63dccf-bcb3-4c3a-860f-925ef5a94180\"],\n" +
             "                    \"dependentId\": \"c7ea1608-eb4b-4859-9bd3-1dd8989e74e6\",\n" +
             "                    \"values\": [\"Yes\"],\n" +
             "                    \"fieldMirror\": \"fa14291d-3d77-41f6-9e09-e720404921af\"\n" +
@@ -35,8 +36,8 @@ public class ScannerRegexReplace {
     static {
         uuidMap.put(UUID.fromString("c7ea1608-eb4b-4859-9bd3-1dd8989e74e6"), UUID.fromString("07d67128-fe2b-11e9-8f0b-362b9e155667"));
         uuidMap.put(UUID.fromString("fa14291d-3d77-41f6-9e09-e720404921af"), UUID.fromString("07d674ac-fe2b-11e9-8f0b-362b9e155667"));
-        uuidMap.put(UUID.fromString("07d675d8-fe2b-11e9-8f0b-362b9e155667"), UUID.fromString("07d67704-fe2b-11e9-8f0b-362b9e155667"));
-        uuidMap.put(UUID.fromString("07d679b6-fe2b-11e9-8f0b-362b9e155667"), UUID.fromString("07d67b00-fe2b-11e9-8f0b-362b9e155667"));
+        uuidMap.put(UUID.fromString("143d3314-43b2-4711-899f-843e223d1128"), UUID.fromString("07d67704-fe2b-11e9-8f0b-362b9e155667"));
+        uuidMap.put(UUID.fromString("db63dccf-bcb3-4c3a-860f-925ef5a94180"), UUID.fromString("07d67b00-fe2b-11e9-8f0b-362b9e155667"));
 
         try {
             jsonNode = objectMapper.readTree(jsonString);
@@ -45,23 +46,33 @@ public class ScannerRegexReplace {
         }
     }
 
-    private static JsonNode updateFieldReferences(JsonNode jsonNode, Map<UUID, UUID> uuidMap) throws JsonProcessingException {
-        String jsonString = jsonNode.toPrettyString();
-        StringBuilder stringBuilder = new StringBuilder(jsonString);
+    private static JsonNode updateFieldReferences(final JsonNode jsonNode, final Map<UUID, UUID> uuidMap) throws JsonProcessingException {
+        final String jsonString = jsonNode.toString();
+        final StringBuilder stringBuilder = new StringBuilder(jsonString);
         final Matcher matcher = uuidRegexPattern.matcher(jsonString);
         while(matcher.find()){
             final String uuidMatch = jsonString.substring(matcher.start(), matcher.end());
-            System.out.println(String.format("\n%s", uuidMatch));
             stringBuilder.replace(matcher.start(), matcher.end(), uuidMap.get(UUID.fromString(uuidMatch)).toString());
         }
-        System.out.println("result is: \n" + stringBuilder.toString());
         return objectMapper.readTree(stringBuilder.toString());
+    }
+
+    private static JsonNode updateFieldReferencesFound(final JsonNode jsonNode, final Map<UUID, UUID> uuidMap) throws JsonProcessingException {
+        final String jsonString = jsonNode.toString();
+        final Matcher matcher = uuidRegexPattern.matcher(jsonString);
+        final StringBuilder result = new StringBuilder();
+        while(matcher.find()){
+            matcher.appendReplacement(result, uuidMap.get(UUID.fromString(matcher.group())).toString());
+        }
+        matcher.appendTail(result);
+        return objectMapper.readTree(result.toString());
     }
 
     public static void main(String[] args) throws JsonProcessingException {
         System.out.println("================= INIT JSON PARSER =================");
         System.out.println(jsonNode.toPrettyString());
-        updateFieldReferences(jsonNode, uuidMap);
+        final JsonNode updatedJsonNode = updateFieldReferencesFound(ScannerRegexReplace.jsonNode, uuidMap);
+        System.out.println(updatedJsonNode.toPrettyString());
         System.out.println("=================  END JSON PARSER =================");
     }
 }
