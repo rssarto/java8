1e503b084e325179ed1bfbaab17addc2353353db
Ricardo Soares Sarto <ricardo.sarto@sinch.com>
2022-07-01 19:52:27 -0300

    Binary Search Tree

---
+++ java8tests-app/src/main/java/test/dataStructures/BinaryTreeTest.java
@@ -0,0 +1,107 @@
+package test.dataStructures;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeTest {
+
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree(new Node(18));
+        int[] unsortedArray = new int[]{3, 7, 16, 3, 2, 9, 20, 45, 86, 4};
+        for (int num : unsortedArray) {
+            binaryTree.add(num);
+        }
+        System.out.println(String.format("Size: %d", binaryTree.getSize()));
+        System.out.println("Sorted list asc: " + binaryTree.sort(BinaryTree.SortDirection.ASC));
+        System.out.println("Sorted list desc: " + binaryTree.sort(BinaryTree.SortDirection.DESC));
+    }
+
+    private static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private static class BinaryTree {
+        private final Node root;
+        private int size = 0;
+
+        public BinaryTree(Node root) {
+            this.root = root;
+            size++;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        private void add(int newValue) {
+            this.add(newValue, this.root);
+        }
+
+        private void add(int newValue, Node node) {
+            if (node.value == newValue) {
+                return;
+            }
+
+            if (newValue < node.value) {
+                if (node.left == null) {
+                    node.left = new Node(newValue);
+                    size++;
+                    return;
+                }
+                add(newValue, node.left);
+            } else {
+                if (node.right == null) {
+                    node.right = new Node(newValue);
+                    size++;
+                    return;
+                }
+                add(newValue, node.right);
+            }
+        }
+
+        private void sort(Node node, List<Integer> listRef) {
+            if (node.left != null) {
+                sort(node.left, listRef);
+            }
+            listRef.add(node.value);
+            if (node.right != null) {
+                sort(node.right, listRef);
+            }
+        }
+
+        private void inverse(Node node, List<Integer> listRef) {
+            if (node.right != null) {
+                inverse(node.right, listRef);
+            }
+            listRef.add(node.value);
+            if (node.left != null) {
+                inverse(node.left, listRef);
+            }
+        }
+
+        public enum SortDirection {
+            ASC, DESC
+        }
+
+        public List<Integer> sort(SortDirection sortDirection) {
+            if (size == 0) {
+                return Collections.emptyList();
+            }
+            final List<Integer> sortedList = new LinkedList<>();
+            if (SortDirection.ASC.equals(sortDirection)) {
+                this.sort(this.root, sortedList);
+            } else {
+                this.inverse(this.root, sortedList);
+            }
+
+            return sortedList;
+        }
+    }
+}
